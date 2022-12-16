c873e5f3d56b3862880b4c1ef57b5aa4218bacd5
Adeyemi <yemiklein77@gmail.com>
2022-12-16 02:07:58 +0100

    highscore

--- starter/script.js
+++ starter/script.js
@@ -2,8 +2,20 @@
 // console.log(document.querySelector('.between').textContent)
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20
-document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+let highscore = 0;
+
+
+document.querySelector('.again').addEventListener ('click', function () {
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#2192FF';
+    document.querySelector('.number').style.width = '15rem';
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+})
 
 document.querySelector('.check').addEventListener ( 
     'click', function () {
@@ -15,7 +27,13 @@ document.querySelector('.check').addEventListener (
             document.querySelector('.message').textContent = '🚫 No Number';
         } else if (guess === secretNumber) {
             document.querySelector('.message').textContent = '🎉 Correct Number';
+            document.querySelector('.number').textContent = secretNumber;
             document.querySelector('body').style.backgroundColor = '#60b347';
+            document.querySelector('.number').style.width = '30rem';
+            if (score > highscore) {
+                highscore = score;
+                document.querySelector('.highscore').textContent = highscore;
+            }
         } else if (guess > secretNumber) {
             if (score > 1) {
             document.querySelector('.message').textContent = '📈 Too High';
